2d71d01d54181a3bae18a9dc8af368376d0ed1d5
Savannah <SavannahTan918@outlook.com>
2022-12-12 20:46:39 -0500

    this is a test

--- 4990_project 1.py
+++ 4990_project 1.py
@@ -3,13 +3,16 @@ from scipy.optimize import line_search
 import numpy
 NORM = numpy.linalg.norm
 import numdifftools as nd
+import matplotlib.pyplot as plt
 
 def func(x): # Objective function
     # return x[0]**4 - 2*x[0]**2*x[1] + x[0]**2 + x[1]**2 - 2*x[0] + 1
     return (1 - x[0])**2 + 100*(x[0]**2 - x[1])**2 + (1 - x[1])**2 + 100*(x[1]**2 - x[2])**2
 # def grad_func(x): # Objective function
 #     return numpy.array([4*x[0]**3-4*x[0]*x[1]+2*x[0]-2, -2*x[0]**2+2*x[1]])
-# NEW VERSIONNNN
+# This is a test
+true_sol = [1, 1, 1]
+
 def Hager_Zhang(Xj, tol, alpha_1, alpha_2):
     x = numpy.zeros((len(Xj), 1))
     for i in range(len(Xj)):
@@ -17,9 +20,11 @@ def Hager_Zhang(Xj, tol, alpha_1, alpha_2):
     grad_func = nd.Gradient(func)
     D = grad_func(Xj)
     delta = -D
-    iter = 0
+    iterations = 0
+    converge = []
     while True:
-        iter += 1
+        iterations += 1
+        converge.append(NORM(true_sol - Xj))
         start_point = Xj # Start point for step length selection 
         beta = line_search(f=func, myfprime=grad_func, xk=start_point, pk=delta, c1=alpha_1, c2=alpha_2)[0] # Selecting the step length
         if beta!=None:
@@ -30,7 +35,9 @@ def Hager_Zhang(Xj, tol, alpha_1, alpha_2):
             for i in range(len(Xj)):
                 x[i] += [Xj[i], ]
 
-            print("iter:", iter)
+            print("iter:", iterations)
+            plt.plot(range(iterations), converge)
+            # plt.show()
             return X, func(X)
         else:
             Xj = X
@@ -43,6 +50,7 @@ def Hager_Zhang(Xj, tol, alpha_1, alpha_2):
             delta = -D + chi*delta
             for i in range(len(Xj)):
                 x[i] += [Xj[i], ]
+
 initial = numpy.random.rand(3)
-x,func_val = Hager_Zhang(initial, 10**-6, 10**-4, 0.2)
+x, func_val = Hager_Zhang(initial, 10**-6, 10**-4, 0.2)
 print(x, func_val)
