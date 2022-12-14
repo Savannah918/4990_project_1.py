f042fcfdb79619a66899d005ce478605e936d5db
MAMBA-ROCKET <barryhan2475328897@gmail.com>
2022-12-14 18:42:48 -0500

    4990_project 1.py

--- 4990_project 1.py
+++ 4990_project 1.py
@@ -3,14 +3,15 @@ from scipy.optimize import line_search
 import numpy as np
 NORM = np.linalg.norm
 import numdifftools as nd
-
+pi = np.pi
 
 ## we need to evaluate the 'conjugate' property
 
 def func(x): # Objective function
-    # return x[0]**4 - 2*x[0]**2*x[1] + x[0]**2 + x[1]**2 - 2*x[0] + 1
-    # return (1 - x[0])**2 + 100*(x[0]**2 - x[1])**2
-    return (1 - x[0])**2 + 100*(x[0]**2 - x[1])**2 + (1 - x[1])**2 + 100*(x[1]**2 - x[2])**2
+    return 10*len(x) + sum(s**2-10*np.cos(2*pi*s) for s in x)                                 #Rastrigin
+    return 0.5 + ((np.sin(x[0]**2-x[1]**2))**2-0.5) / (1+0.001*(x[0]**2+x[1]**2))**2             #Schaffer
+    return 1 + 1/4000 * (x[0]**2+x[1]**2) - np.cos(x[0])*np.cos(x[1]/np.sqrt(2))                   #Griewank
+    return (1 - x[0])**2 + 100*(x[0]**2 - x[1])**2 + (1 - x[1])**2 + 100*(x[1]**2 - x[2])**2         #Rosenbrock
 # def grad_func(x): # Objective function
 #     return numpy.array([4*x[0]**3-4*x[0]*x[1]+2*x[0]-2, -2*x[0]**2+2*x[1]])
 
@@ -48,6 +49,6 @@ def Hager_Zhang(Xj, tol, alpha_1, alpha_2):
             for i in range(len(Xj)):
                 x[i] += [Xj[i], ]
 
-Initial_data = np.random.rand(3)
+Initial_data = [5,5]
 x,func_val = Hager_Zhang(Initial_data, 10**-6, 10**-4, 0.2)
 print(x, func_val)
